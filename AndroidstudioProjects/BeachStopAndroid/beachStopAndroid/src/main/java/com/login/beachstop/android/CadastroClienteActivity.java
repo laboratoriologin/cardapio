25564eb717bef3693f4ccd547cc24d32ed5f2935
Marcelo <marcelomagalhaes31@gmail.com>
2015-02-23 15:42:33 -0300

    ...

--- AndroidstudioProjects/BeachStopAndroid/beachStopAndroid/src/main/java/com/login/beachstop/android/CadastroClienteActivity.java
+++ AndroidstudioProjects/BeachStopAndroid/beachStopAndroid/src/main/java/com/login/beachstop/android/CadastroClienteActivity.java
@@ -5,6 +5,8 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
@@ -28,6 +30,7 @@ import com.login.beachstop.android.utils.Constantes;
 import org.json.JSONException;
 
 import java.util.Arrays;
+import java.util.Calendar;
 
 import static com.login.beachstop.android.R.layout.activity_cadastro_cliente;
 
@@ -45,6 +48,62 @@ public class CadastroClienteActivity extends DefaultActivity {
     private Cliente cliente;
     private LoginButton loginButton;
     private UiLifecycleHelper uiHelper;
+    private TextWatcher textWatcher = new TextWatcher() {
+
+        private String current = "";
+        private String ddmmyyyy = "DDMMAAAA";
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (!s.toString().equals(current)) {
+                String clean = s.toString().replaceAll("[^\\d.]", "");
+                String cleanC = current.replaceAll("[^\\d.]", "");
+                Calendar cal = Calendar.getInstance();
+                int cl = clean.length();
+                int sel = cl;
+                for (int i = 2; i <= cl && i < 6; i += 2) {
+                    sel++;
+                }
+                //Fix for pressing delete next to a forward slash
+                if (clean.equals(cleanC)) sel--;
+
+                if (clean.length() < 8){
+                    clean = clean + ddmmyyyy.substring(clean.length());
+                }else{
+                    //This part makes sure that when we finish entering numbers
+                    //the date is correct, fixing it otherwise
+                    int day  = Integer.parseInt(clean.substring(0,2));
+                    int mon  = Integer.parseInt(clean.substring(2,4));
+                    int year = Integer.parseInt(clean.substring(4,8));
+
+                    if(mon > 12) mon = 12;
+                    cal.set(Calendar.MONTH, mon-1);
+                    day = (day > cal.getActualMaximum(Calendar.DATE))? cal.getActualMaximum(Calendar.DATE):day;
+                    year = (year<1900)?1900:(year>2100)?2100:year;
+                    clean = String.format("%02d%02d%02d",day, mon, year);
+                }
+
+                clean = String.format("%s/%s/%s", clean.substring(0, 2),
+                        clean.substring(2, 4),
+                        clean.substring(4, 8));
+                current = clean;
+                editTextAniversario.setText(current);
+                editTextAniversario.setSelection(sel < current.length() ? sel : current.length());
+            }
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            // TODO Auto-generated method stub
+
+        }
+    };
     private ResponseListener listenerCadastroCliente = new ResponseListener() {
         @Override
         public void onResult(ServerResponse serverResponse) {
@@ -110,6 +169,7 @@ public class CadastroClienteActivity extends DefaultActivity {
         this.editTextEmail = (EditText) findViewById(R.id.activity_cadastro_cliente_edit_text_email);
         this.editTextCelular = (EditText) findViewById(R.id.activity_cadastro_cliente_edit_text_celular);
         this.editTextAniversario = (EditText) findViewById(R.id.activity_cadastro_cliente_edit_text_data_nascimento);
+        this.editTextAniversario.addTextChangedListener(this.textWatcher);
         this.loginButton = (LoginButton) findViewById(R.id.fb_login_button);
 
         loginButton.setReadPermissions(Arrays.asList("public_profile"));
